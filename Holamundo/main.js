afee2d6c3b1bcd6edd4356067467779be7c127e6
EdsonRosales97 <erosales@applab.mx>
2021-07-07 18:15:51 -0500

    First commit

---
+++ Holamundo/main.js
@@ -0,0 +1,25 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            titulo: 'Mi banco con Vue.js',
+            cantidad: 1000,
+            enlace: 'https://youtube.com/bluuweb',
+            estado: true,
+            servicios: ['Transferencias', 'Pagos', 'Giros', 'Cheques'],
+            desactivar: false,
+        }
+    },
+    methods: {
+        agregarSaldo() {
+            this.cantidad = this.cantidad + 100
+        },
+        disminuirSaldo(valor) {
+            if(this.cantidad === 0) {
+                this.desactivar = true;
+                alert('Saldo en 0');
+                return
+            }
+            this.cantidad = this.cantidad - valor;
+        }
+    }
+})
